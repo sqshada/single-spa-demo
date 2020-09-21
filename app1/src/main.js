79143594fa418d3ec011372db2e6669ba062273a
chenle <chenle@forchange.tech>
2020-09-21 15:10:35 +0800

    feat: add demo

---
+++ app1/src/main.js
@@ -0,0 +1,39 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router/index'
+import singleSpaVue from 'single-spa-vue'
+
+Vue.config.productionTip = false
+
+const appOptions = {
+  el: '#microApp',
+  router,
+  render: h => h(App)
+}
+
+// 支持应用独立运行、部署，不依赖于基座应用
+if (!window.singleSpaNavigate) {
+  delete appOptions.el
+  new Vue(appOptions).$mount('#app')
+}
+
+// 基于底座应用，导出生命周期函数
+const vueLifecycle = singleSpaVue({
+  Vue,
+  appOptions
+})
+
+export function bootstrap(props) {
+  console.log('app1 bootstrap', props)
+  return vueLifecycle.bootstrap(() => {})
+}
+
+export function mount(props) {
+  console.log('app1 mount', props)
+  return vueLifecycle.mount(() => {})
+}
+
+export function unmount(props) {
+  console.log('app1 unmount', props)
+  return vueLifecycle.unmount(() => {})
+}
---
+++ layout/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/app1">app1</router-link> |
+      <router-link to="/app2">app2</router-link>
+    </div>
+    <!-- 子应用容器 -->
+    <div id = "microApp">
+      <router-view/>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "App",
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
